506604f681481904d2d20d2fef73f3ff9cc14c96
C-Raptor <45814998+C-Raptor@users.noreply.github.com>
2018-12-12 12:01:06 +0300

    Add files via upload

---
+++ asciicode.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+//#include <clocale>
+#include <time.h>
+
+#define N  62
+main()
+{
+	char a[10];
+	scanf("%s", &a);
+	printf("%s", a);
+	getch();
+}
